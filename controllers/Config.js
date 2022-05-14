ac4daad1d6f48186281e382efb7b04c44d979c6f
JuanRi <iesjuanricardo@gmail.com>
2022-05-14 15:12:55 -0500

    Cree la conexion a MySQL

--- controllers/Config.js
+++ controllers/Config.js
@@ -11,4 +11,5 @@ export default{
     password: process.env.PASSWORD
 
 }
+
 console.log(process.env.PORT, process.env.HOST, process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, process.env.CONNECTION)
---
+++ controllers/ControlConnection.js
@@ -0,0 +1,32 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+
+// Create connection to database
+var config = {  
+  server: 'localhost',  //update me
+  authentication: {
+      type: 'default',
+      options: {
+          userName: 'hoja', //update me
+          password: '1234'  //update me
+      }
+  },
+  options: {
+      // If you are on Microsoft Azure, you need encryption:
+      //encrypt: true,
+      database: 'hoja',
+      trustServerCertificate: true  //update me
+  }
+};  
+var connection = new Connection(config);  
+connection.on('connect', function(err) {  
+  // If no error, then good to proceed.
+  if(err){
+  console.log("Connected err", err);
+  }else{
+    console.log('connected');
+  }  
+});
+
+connection.connect();
